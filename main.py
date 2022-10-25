f3f30c111b94cb120dda66f14d00734caa68e818
RDMerlo <68595137+RDMerlo@users.noreply.github.com>
2022-10-25 19:47:33 +0000

    Сделал первые 2 пункта

--- main.py
+++ main.py
@@ -69,5 +69,76 @@ for k in reversed(range(0, n-1, 1)):
     if (X[i] is not None):
       sum_row += A[k][i] * X[i]
   X[k] = F[k] - sum_row
-print("\n\nX = ",X)
+print("\n\nX = ",X,"\n\n")
 
+#вектор невязки
+r = np.dot(A, X) - F
+print("Вектор невязки: ",r,"\n\n")
+
+#определитель
+L = A_orig
+L = np.column_stack((L, F))
+print("L:\n\n",L,"\n")
+for k in range(0, 4, 1):
+  # вернём индекс максимального элемента по модулю из столбца
+  index_max = np.argmax(abs(L[k:,k])) + k
+  # print(A[index_max][k],"  ", abs(A[k:,k]))
+  #меняем строки местами
+  swap_row(L, k, index_max)
+  # print(A,"\n") 
+  # L[k] /= L[k][k]
+  for i in range(k+1, n, 1):
+      L[i] = L[i] - L[k] * L[i][k]
+    
+print("L:\n\n",L,"\n")  
+
+void GetDeterminant(double **matrix, int size, double epsilon)
+{
+    //Предполагается, что матрица квадратная
+    int pivot_index = -1;
+    double pivot_value = 0;
+    double determinant = 1;
+    
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = i; j < size; j++)
+        {
+            if(abs(matrix[j][i]) > pivot_value)
+            {
+                pivot_index = j;
+                pivot_value = abs(matrix[j][i]);
+            }
+        }
+        
+        //Если опорный элемент равен нулю (эпсилон для сброса погрешности)
+        if(pivot_value < epsilon)
+        {
+            //Матрица вырождена
+            return 0;
+        }
+        
+        if(pivot_index != i)
+        {
+            //Обменяем строки местами
+            SwapRows(matrix, pivot_index, i)
+            determinant *= -1;
+        }
+        
+        for(int j = i + 1; j < size; j++)
+        {
+            if(matrix[j][i] != 0)
+            {
+                multiplier = 1 / matrix[i][i] * matrix[j][i];
+                
+                for(int k = i; k < size; k++)
+                {
+                    matrix[j][k] -= matrix[i][k] * multiplier;
+                }
+            }
+        }
+        
+        determinant *= matrix[i][i];
+    }
+    
+    return determinant;
+}
